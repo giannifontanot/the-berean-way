8fdcda045d422d56f95d432857fe647dec311116
giannifontanotAA <93943543+giannifontanotAA@users.noreply.github.com>
2026-06-14 07:26:08 -0500

    Paquito added

---
+++ sites/s-e6c057f3/data.js
@@ -0,0 +1,20 @@
+/* Usuario: Paquito  (carpeta: s-e6c057f3) */
+window.STUDENT_SITE = {
+  title: "Los estudios de Paquito",
+  student: "Paquito",
+  headerImage: "img/header.png",
+  footer: {
+    image: "img/footer.png",
+  },
+  pages: [
+    {
+      slug: "estudio-1",
+      title: "Estudio 1",
+      type: "accordion",
+      purpose: "Edita este archivo para poner el contenido de Paquito.",
+      items: [
+        { heading: "Primer tema", body: "<p>Escribe aquí el contenido.</p>" }
+      ]
+    }
+  ]
+};
